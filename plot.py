0082726db65891e3a300c854690ed4540c55c5ad
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2023-05-14 02:27:35 +0530

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Create a line chart
+plt.plot(x, y)
+
+# Set the title and axis labels
+plt.title("Line Chart")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+
+# Show the chart
+plt.show()
